cd1fceca8afc828b1ceced2ca06fe9db1de1b04e
shikhars371 <shikhars371@gmail.com>
2021-03-05 11:43:54 +0530

    var changes

--- example.js
+++ example.js
@@ -1,7 +1,7 @@
 const derive = require('./')
 
 const ns = 'test'
-const input = 'google.com'
+const input = 'dwebx.org'
 const masterKey = Buffer.alloc(32)
 
 const out = derive(ns, masterKey, input)
